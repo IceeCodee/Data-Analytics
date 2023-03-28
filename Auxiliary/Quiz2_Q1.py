2e899aa817a5e624cdd0eda610ea69491ef75888
IceeCodee <86699334+IceeCodee@users.noreply.github.com>
2023-03-28 16:25:48 -0400

    Update Quiz2_Q1.py
    formatted

--- Auxiliary/Quiz2_Q1.py
+++ Auxiliary/Quiz2_Q1.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 11:46:21 2023
 
-@author: taylor headen
-Quiz 2 question 2
+Quiz 2 
+    question 2
+
+Call factorial function (given below) in the definition of a function fac_list() that takes a list of integers, ls, and
+two integer arguments, low (default 0) and high (default 10). It returns the list of factorials of
+the items in ls that are between low and high inclusive. (The items in ls, not their factorials,
+are between low and high inclusive.) 
 """
 
 def factorial(n):
-
     if n <= 0:
-
         return 1
-
     return n * factorial(n-1)
 
 def fac_list(ls, low =0, high =10):
